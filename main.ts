0cf215d408601edcaf0a2f2f85330d4d67de31c6
irj1001 <74235665+irj1001@users.noreply.github.com>
2022-06-08 01:47:02 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,28 @@
 namespace lasertag_master {
 
-    //% block
-    export function setup_master() {
-        
+    //% block="setup |lives $lives|"
+    export function setup_master(lives: number) {
+        radio.setTransmitPower(1)
+        let team=1
+        input.onButtonPressed(Button.A, function() {
+            if (team==1){
+                team=2
+            }
+            else{team=1}
+        })
 
+        basic.forever(function () {
+
+            basic.showNumber(team)
+            radio.setGroup(team)
+
+
+
+        })
     }
 
+
+    
+
 }
+
